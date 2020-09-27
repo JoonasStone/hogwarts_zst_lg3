f0f8d448d4a9e1ac9f39d6e6aa0c2a521c6bfa83
JoonasStone <361825495@qq.com>
2020-09-28 00:46:45 +0800

    commit readme

--- lagou_game/README.md
+++ lagou_game/README.md
@@ -1,2 +1,3 @@
 #first learn by lagou_hogwarts \
 #I will do a practice for my first python game
+./hogwarts_zst_lg3/lagou_game/src
---
+++ lagou_game/src/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+'''
+@File    :   __init__.py.py
+@License :   (C)Copyright 
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2020/9/28 0:42   Joonas      1.0         None
+'''
